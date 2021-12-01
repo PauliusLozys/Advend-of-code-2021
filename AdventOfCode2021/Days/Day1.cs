3200eee082739f652063016ca9d0e155adc3faab
RainbowDog98 <42966213+RainbowDog98@users.noreply.github.com>
2021-12-01 09:18:48 +0200

    Initial commit

---
+++ AdventOfCode2021/Days/Day1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Days
+{
+    class Day1
+    {
+        public static void PartOne()
+        {
+            var lines = File.ReadAllLines("../../../Days/Input1.txt");
+
+            int increaseCount = 0;
+
+            int currentNumber = int.Parse(lines[0]);
+
+            foreach (var line in lines[1..])
+            {
+                var current = int.Parse(line);
+
+                if (current > currentNumber)
+                    increaseCount++;
+
+                currentNumber = current;
+            }
+
+            Console.WriteLine($"Part 1: depth increased {increaseCount} times");
+        }
+
+        public static void PartTwo()
+        {
+            var lines = File.ReadAllLines("../../../Days/Input1.txt");
+
+            int increaseCount = 0;
+            int currentNumber = 0;
+
+            for (int j = 0; j < 3; j++)
+            {
+                var value = int.Parse(lines[j]);
+                currentNumber += value;
+            }
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < (i + 3) && j < lines.Length; j++)
+                {
+                    var value = int.Parse(lines[j]);
+                    sum += value;
+                }
+
+                if (sum > currentNumber)
+                    increaseCount++;
+
+                currentNumber = sum;
+            }
+
+            Console.WriteLine($"Part 2: depth increased {increaseCount} times");
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2021.Days;
+using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day1.PartOne();
+            Day1.PartTwo();
+        }
+    }
+}
